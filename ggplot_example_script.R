9dc83580988f1ceaecd166687804c3237fe093e6
ramakanth7618 <ramakanth7618@gmail.com>
2015-02-22 14:45:32 -0700

    Commitedd

---
+++ ggplot_example_script.R
@@ -0,0 +1,7 @@
+# ggplot learning script
+# ramakanth Reddy Pallavali --Rpallavali@email.arizona.edu
+# February 22 2015
+install.packages("ggplot2",dependencies = TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2"
